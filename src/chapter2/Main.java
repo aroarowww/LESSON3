5ded9dc8d6bccc486885a8cc18ec2cccde650ad4
choshibi <115560399+aroarowww@users.noreply.github.com>
2024-03-26 14:02:04 +0900

    質問用再提出

--- src/chapter2/Main.java
+++ src/chapter2/Main.java
@@ -1,25 +1,46 @@
 package chapter2;
 
 import java.util.Scanner;
-import chapter2.DayOfWeek;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.print("曜日を入力して下さい：");
 		String input = new Scanner(System.in).nextLine();
 		DayOfWeek week = DayOfWeek.valueOf(input);
+		//input.close();
+
 		// 以下に処理を記述
-//		キーボードからの入力で曜日名を受け取り、該当するenumインスタンスを取得して、
+//		キーボードからの入力で曜日名を受け取り、該当する/インスタンスを取得して、
 //		曜日の日本語名を出力してください。
 		switch (week) {
-		case Sun: {
-			System.out.println(DayOfWeek.getWeekDay("sun") + "日です。");
+		case Sun: 
+			System.out.println(week.getWeekDay("sun") + "日です。");//enu.mはインスタンスなしで使えない、
+																	//そのためここでDayOfWeek.getWeekDay("sun")はエラーが出る
 			break;
-		}
-		case Mon: {
-			System.out.println(DayOfWeek.getWeekDay("mon") + "日です。");
+		
+		case Mon: 
+			System.out.println(week.getWeekDay("mon") + "日です。");
 			break;
-		}
+		
+		case Tue: 
+			System.out.println(week.getWeekDay("sun") + "日です。");
+			break;
+		
+		case Wed: 
+			System.out.println(week.getWeekDay("mon") + "日です。");
+			break;
+		
+		case Thu: 
+			System.out.println(week.getWeekDay("sun") + "日です。");
+			break;
+		
+		case Fri: 
+			System.out.println(week.getWeekDay("mon") + "日です。");
+			break;
+		
+		case Sat: 
+			System.out.println(week.getWeekDay("sun") + "日です。");
+			break;
 		}
 	}
 }
